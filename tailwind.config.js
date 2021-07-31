109c5ecac7712baf398871d7c35b7b4630771777
Everett Blakley <everettblakley@gmail.com>
2021-07-30 23:28:26 -0600

    Modifying tailwind purge settings to keep custom color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,38 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: {
+    content: ["./index.html", "./src/**/*.{vue,js}"],
+    safelist: [
+      "bg-times-15",
+      "bg-times-20",
+      "bg-times-25",
+      "bg-times-30",
+      "bg-times-35",
+      "bg-times-40",
+      "bg-times-45",
+      "bg-times-60",
+      "bg-times-90",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
         times: {
-          '15': '#64AC94',
-          '20': '#A1AB4B',
-          '25': '#FEAD4B',
-          '30': '#E16424',
-          '35': '#A05B3A',
-          '40': '#56362B',
-          '45': '#9B1D28',
-          '60': '#AE3271',
-          '90': '#602042'
-        }
+          15: "#64AC94",
+          20: "#A1AB4B",
+          25: "#FEAD4B",
+          30: "#E16424",
+          35: "#A05B3A",
+          40: "#56362B",
+          45: "#9B1D28",
+          60: "#AE3271",
+          90: "#602042",
+        },
       },
       fontFamily: {
-        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans]
+        sans: ['"Open Sans"', ...defaultTheme.fontFamily.sans],
       },
     },
   },
@@ -27,4 +40,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
